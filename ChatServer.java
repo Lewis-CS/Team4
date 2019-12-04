6733ebf39df20fb123a92325f9551c29f16acd96
Ryan Schefske <ryanschefske@gmail.com>
2019-12-04 00:26:51 -0600

    TM4FA19-24: Added welcome message, instructions, and stabilized rooms

--- ChatServer.java
+++ ChatServer.java
@@ -6,8 +6,15 @@ class ChatServer implements Runnable{
 
   Socket connectionSocket;
   public static Vector clients = new Vector();
+  public static Vector rooms = new Vector();
 
   public ChatServer(Socket s){
+
+    if (rooms.contains("General")) {
+    } else {
+      rooms.add("General");
+    }
+
     try{
     	System.out.println("New client connected");
     	connectionSocket=s;
@@ -17,8 +24,8 @@ class ChatServer implements Runnable{
   }
 
   public void run(){
-    try{
 
+    try{
     	BufferedReader reader =
     		new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
     	BufferedWriter writer=
@@ -27,16 +34,47 @@ class ChatServer implements Runnable{
 
       while(true) {
         String data1 = reader.readLine().trim();
-        System.out.println("Received New Message: " + data1);
 
-        for (int i = 0; i < clients.size(); i++) {
-          try {
-            BufferedWriter bw = (BufferedWriter)clients.get(i);
-            bw.write(data1);
-            bw.write("\r\n");
-            bw.flush();
-          } catch (Exception e) {
-            e.printStackTrace();
+        if (data1.contains("$create")) {
+          String[] args = data1.split(" ");
+          if (args.length < 4) {
+            System.out.println("Not enough arguments");
+          } else {
+            rooms.add(args[4]);
+            System.out.println(rooms);
+            for (int i = 0; i < clients.size(); i++) {
+              try {
+                BufferedWriter bw = (BufferedWriter)clients.get(i);
+                bw.write("New room created: " + args[4]);
+                bw.write("\r\n");
+                bw.flush();
+              } catch (Exception e) {
+                e.printStackTrace();
+              }
+            }
+          }
+        } else if (data1.contains("$list")) {
+          for (int i = 0; i < clients.size(); i++) {
+            try {
+              BufferedWriter bw = (BufferedWriter)clients.get(i);
+              bw.write("Available Rooms: " + rooms);
+              bw.write("\r\n");
+              bw.flush();
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+          }
+        } else {
+          System.out.println("Received New Message: " + data1);
+          for (int i = 0; i < clients.size(); i++) {
+            try {
+              BufferedWriter bw = (BufferedWriter)clients.get(i);
+              bw.write(data1);
+              bw.write("\r\n");
+              bw.flush();
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
           }
         }
       }
--- ChatWindow.java
+++ ChatWindow.java
@@ -9,14 +9,15 @@ public class ChatWindow implements Runnable {
 
     public JTextField textField;
     public JTextArea textArea;
-    public String login;
+    public JPanel sidePanel;
+    public String currentRoom;
     BufferedWriter writer;
     BufferedReader reader;
     ArrayList<String> myRooms = new ArrayList<String>();
-    //ArrayList<Rooms> rooms = new ArrayList<Rooms>();
 
-    public ChatWindow(String user){
-        login = user;
+    public ChatWindow(String user) {
+      currentRoom = "General";
+      myRooms.add("General");
 
         JFrame frame = new JFrame("Chat");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -29,11 +30,14 @@ public class ChatWindow implements Runnable {
         JPanel panel2 = new JPanel();
         panel2.setLayout(new BorderLayout());
 
-        // TODO: Add side panel again
-        JPanel sidePanel = new JPanel();
+        sidePanel = new JPanel();
         sidePanel.setLayout(new BoxLayout(sidePanel, BoxLayout.Y_AXIS));
         JLabel rooms = new JLabel("Rooms:                     ");
+        JLabel generalRoom = new JLabel("General");
+        generalRoom.setForeground(Color.gray);
         sidePanel.add(rooms);
+        sidePanel.add(new JLabel(" "));
+        sidePanel.add(generalRoom);
         panel2.add(sidePanel, BorderLayout.WEST);
 
         textField = new JTextField();
@@ -46,34 +50,23 @@ public class ChatWindow implements Runnable {
         JButton clearButton = new JButton("Clear");
         clearButton.setOpaque(true);
         clearButton.setForeground(Color.red);
-        JButton addRoom = new JButton("Add Room");
-        addRoom.setOpaque(true);
-        addRoom.setForeground(Color.gray);
 
         buttonPanel.add(sendButton);
         buttonPanel.add(clearButton);
-        buttonPanel.add(addRoom);
         panel1.add(buttonPanel, BorderLayout.EAST);
 
         textArea = new JTextArea();
         textArea.setEditable(false);
-        textArea.setText("Current Room: General\n");
         panel2.add(textArea, BorderLayout.CENTER);
         panel2.add(panel1, BorderLayout.SOUTH);
 
-        myRooms.add("General");
-        JButton button = new JButton("General");
-        button.setOpaque(false);
-        button.setContentAreaFilled(false);
-        button.setBorderPainted(false);
-        sidePanel.add(button);
-        button.addActionListener(new ActionListener() {
-          public void actionPerformed(ActionEvent ev) {
-            textArea.setText("");
-            String thisRoom = myRooms.get(0);
-            textArea.setText("Current Room: " + thisRoom + "\n");
-          }
-        });
+        //Welcome message
+        textArea.append("Welcome to the Chat, " + user + "!\n");
+        textArea.append("\nInstructions: \n");
+        textArea.append("\'$create [ROOM_NAME]\' to create a new room\n");
+        textArea.append("\'$join [ROOM_NAME]\' to join a new room\n");
+        textArea.append("\'$list\' to list existing room names\n");
+        textArea.append("\nHave fun!\n\n");
 
         frame.setContentPane(panel2);
 
@@ -81,21 +74,42 @@ public class ChatWindow implements Runnable {
           Socket socketClient = new Socket("localhost", 4444);
           writer = new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));
           reader = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+
+          writer.write(user + " has joined the chat.");
+          writer.write("\r\n");
+          writer.flush();
+
         } catch (Exception e) {
           e.printStackTrace();
         }
 
         Action sendAction = new AbstractAction() {
           public void actionPerformed(ActionEvent ev) {
-            String message = login + " > " + textField.getText();
-            textField.setText("");
-            try {
-              writer.write(message);
-              writer.write("\r\n");
-              writer.flush();
-            } catch (Exception e) {
-              e.printStackTrace();
+            String message = textField.getText();
+
+            if (message.contains("$join")) {
+              String[] x = message.split(" ");
+              if (x.length >= 2 && currentRoom != x[1]) {
+                if (myRooms.contains(x[1])) {
+                  currentRoom = x[1];
+                  textArea.append("You have joined " + currentRoom + "\n");
+                } else {
+                  textArea.append("That room does not exist\n");
+                }
+              } else {
+                textArea.append("Could not join room\n");
+              }
+            } else {
+              message = currentRoom + " " + user + " > " + textField.getText();
+              try {
+                writer.write(message);
+                writer.write("\r\n");
+                writer.flush();
+              } catch (Exception e) {
+                e.printStackTrace();
+              }
             }
+            textField.setText("");
           }
         };
         textField.addActionListener(sendAction);
@@ -107,42 +121,6 @@ public class ChatWindow implements Runnable {
           }
         });
 
-        addRoom.addActionListener(new ActionListener() {
-          public void actionPerformed(ActionEvent ev) {
-              String s = (String)JOptionPane.showInputDialog(
-              frame,
-              "Enter Name of New Room: ",
-              "New Room",
-              JOptionPane.PLAIN_MESSAGE
-            );
-
-            if ((s != null) && (s.length() > 0)) {
-              myRooms.add(s);
-              sidePanel.removeAll();
-              JLabel rooms = new JLabel("Rooms:                     ");
-              sidePanel.add(rooms);
-              for (int j = 0; j < myRooms.size(); j++) {
-                final int num = j;
-                JButton button = new JButton(myRooms.get(j));
-                button.setOpaque(false);
-                button.setContentAreaFilled(false);
-                button.setBorderPainted(false);
-                sidePanel.add(button);
-                button.addActionListener(new ActionListener() {
-                  public void actionPerformed(ActionEvent ev) {
-                    textArea.setText("");
-                    String thisRoom = myRooms.get(num);
-                    textArea.setText("Current Room: " + thisRoom + "\n");
-                  }
-                });
-                textArea.setText("");
-                textArea.setText("Current Room: " + s + "\n");
-              }
-              return;
-            }
-          }
-        });
-
         frame.setVisible(true);
     }
 
@@ -150,7 +128,26 @@ public class ChatWindow implements Runnable {
       try {
         String serverMsg = "";
         while ((serverMsg = reader.readLine()) != null) {
-          textArea.append(serverMsg + "\n");
+          if (serverMsg.contains("has joined the chat")) {
+            textArea.append(serverMsg + "\n");
+          } else if (serverMsg.contains("Available Rooms:")) {
+            textArea.append(serverMsg + "\n");
+          } else if (serverMsg.contains("New room created:")) {
+            textArea.append(serverMsg + "\n");
+            String[] x = serverMsg.split(": ");
+            myRooms.add(x[1]);
+            JLabel newRoom = new JLabel(x[1]);
+            newRoom.setForeground(Color.gray);
+            sidePanel.add(newRoom);
+            sidePanel.validate();
+            sidePanel.repaint();
+            System.out.println(myRooms);
+          } else {
+            String[] splitMessage = serverMsg.split(" ", 2);
+            if (splitMessage[0].equals(currentRoom)) {
+              textArea.append(splitMessage[1] + "\n");
+            }
+          }
         }
       } catch (Exception e) {
         e.printStackTrace();
